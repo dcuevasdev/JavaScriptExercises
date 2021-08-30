b1c0271c138695890edf4242ec80231615e77eb6
dcuevas24 <ivandaniel1224@gmail.com>
2021-08-30 11:14:58 -0500

    Solucion del primer ejercicio

---
+++ Ejercicio_1/index.js
@@ -0,0 +1,7 @@
+let age = Number(prompt("¿Cuántos años tienes?"));
+
+if (age >= 18) {
+  alert("Eres mayor de edad y puedes conducir");
+} else {
+  alert("Eres menor de edad y NO puedes conducir");
+}
