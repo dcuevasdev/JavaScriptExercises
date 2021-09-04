7ba5ab99e03b2b41d38a8e6f71b63110819106ec
dcuevas24 <ivandaniel1224@gmail.com>
2021-09-04 09:33:37 -0500

    Solucion del quinto ejercicio

---
+++ Ejercicio_5/index.js
@@ -0,0 +1,10 @@
+let dataUser = prompt("Ingresa el número de iteraciones que desees");
+
+for (let i = dataUser; i >= 1; i--) {
+  document.write(i);
+  5;
+  for (let rep = i - 1; rep >= 1; rep--) {
+    document.write(i);
+  }
+  document.write("<br>");
+}
