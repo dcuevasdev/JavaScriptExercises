590c17ba924b5a9ca0303756bf05fa72e20d7b1c
dcuevas24 <ivandaniel1224@gmail.com>
2021-09-16 17:41:44 -0500

    Correcion ejercicio 11

--- Ejercicio_11/index.js
+++ Ejercicio_11/index.js
@@ -3,22 +3,22 @@ const results = [1, "X", 2];
 let sixtyPercent = 0;
 let thirtyPercent = 0;
 let tenPercent = 0;
-let incremet = 1;
+let increment = 1;
 
 for (let i = 1; i <= 50; i++) {
   let random = Math.floor(Math.random() * results.length);
 
   if (random === 0 && sixtyPercent <= 7) {
     sixtyPercent++;
-    document.write(`Resultado ${incremet}: ${results[random]} <br>`);
-    incremet++;
+    document.write(`Resultado ${increment}: ${results[random]} <br>`);
+    increment++;
   } else if (random === 1 && thirtyPercent <= 3) {
     thirtyPercent++;
-    document.write(`Resultado ${incremet}: ${results[random]} <br>`);
-    incremet++;
+    document.write(`Resultado ${increment}: ${results[random]} <br>`);
+    increment++;
   } else if (random === 2 && tenPercent <= 1) {
     tenPercent++;
-    document.write(`Resultado ${incremet}: ${results[random]} <br>`);
-    incremet++;
+    document.write(`Resultado ${increment}: ${results[random]} <br>`);
+    increment++;
   }
 }
