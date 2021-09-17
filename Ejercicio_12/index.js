ddf7bcbdeef16ae38782bb3b38f7cc0fe3db5b39
dcuevas24 <ivandaniel1224@gmail.com>
2021-09-17 08:01:33 -0500

    Resolver bug del ejercicio 12

--- Ejercicio_12/index.js
+++ Ejercicio_12/index.js
@@ -1,5 +1,15 @@
 const dataUser = prompt("Ingresa un texto");
+const sizeDataUser = dataUser.length;
 
-for (let lyrics of dataUser) {
+/*for (let lyrics of dataUser) {
   document.write(lyrics + "-");
+}*/
+
+for (let i = 0; i < sizeDataUser; i++) {
+  let lyric = dataUser.charAt(i);
+  if (i == sizeDataUser - 1) {
+    document.write(lyric);
+  } else {
+    document.write(lyric + "-");
+  }
 }
