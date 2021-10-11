855faef9bae50f33b1ec6860475f2bede2a29586
dcuevas24 <ivandaniel1224@gmail.com>
2021-10-11 10:23:12 -0500

    Solucion ejercicio 22 para la pregunta 2

--- Ejercicio_22/index.js
+++ Ejercicio_22/index.js
@@ -1,15 +1,12 @@
 const values = [true, 5, false, "hola", "adios", 2];
 
+//Texto mayor
 const findString = values.filter((value) => {
   return typeof value == "string";
 });
 
 const findNumber = values.filter((value) => {
   return typeof value == "number";
-});
-
-const findBoolean = values.filter((value) => {
-  return typeof value == "boolean";
 });
 
 function stringGreatest() {
@@ -20,8 +17,25 @@ function stringGreatest() {
   document.write(
     `La cadena de texto más larga es <strong>${
       order[order.length - 1]
-    }</strong> `
+    }</strong> <br/>`
   );
 }
 
 stringGreatest();
+
+//Booleanos
+
+const findBoolean = values.filter((value) => {
+  return typeof value == "boolean";
+});
+
+function booleans() {
+  for (const element of findBoolean) {
+    if (element === true) {
+      document.write(`<strong>${findBoolean[0]}</strong> <br/>`);
+    } else {
+      document.write(`<strong>${findBoolean[1]}</strong> <br/>`);
+    }
+  }
+}
+booleans();
