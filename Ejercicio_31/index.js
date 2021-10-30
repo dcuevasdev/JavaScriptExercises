75f2baf7fdc06b244552a562bc8523b416ef11a9
dcuevas24 <ivandaniel1224@gmail.com>
2021-10-30 11:56:35 -0500

    Solucion ejercicio 31

---
+++ Ejercicio_31/index.js
@@ -0,0 +1,14 @@
+/*
+Verificar vacío
+Queremos que la función isEmpty devuelva true si el string está vacío, y false si no lo está
+*/
+
+function isEmpty(str) {
+  if (str == "") {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+isEmpty("dvfszdvsd");
