384f9ba4af29e3531c48a65b79b8b36de496b792
dcuevas24 <ivandaniel1224@gmail.com>
2021-11-06 07:25:56 -0500

    Solución ejercicio 42

---
+++ Ejercicio_42/index.js
@@ -0,0 +1,28 @@
+/*
+Mayor repetido
+Queremos saber cuantas veces se repite el número más grande de un array. El valor que tiene que devolver la función es un número
+*/
+
+const listNumbers = [
+  100, 5, 20, 100, 3, 6, 100, 9, 8, 100, 96, -100, -36, -4, 100, 12, 58, 62,
+];
+let counter = 0;
+
+const orderNumbers = listNumbers.sort((a, b) => {
+  return a - b;
+});
+
+function countGreater(arr) {
+  const list = arr;
+  const lastPosition = list[list.length - 1];
+
+  for (const number of list) {
+    if (number === lastPosition) {
+      counter++;
+    }
+  }
+
+  console.log(counter);
+}
+
+countGreater(orderNumbers);
