88a081d819a0c50e2cda35da99e2563e914a1e20
dcuevas24 <ivandaniel1224@gmail.com>
2021-11-16 09:51:35 -0500

    Confirmación del dinero disponible en el cajero

---
+++ Ejercicio_45/index.js
@@ -0,0 +1,17 @@
+const money = {
+  100: 3,
+  50: 6,
+  20: 10,
+  10: 50,
+  1: 50,
+};
+
+const denomination = Object.keys(money);
+
+const availableMoney = () => {
+  const available = denomination.map((value) => value * money[value]);
+
+  const totalMoney = available.reduce((accum, current) => accum + current);
+
+  return totalMoney;
+};
