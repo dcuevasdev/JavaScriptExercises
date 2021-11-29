67174894b166fffd2c05851c7659983644913113
dcuevas24 <ivandaniel1224@gmail.com>
2021-11-29 09:42:44 -0500

    Solución ejercicio 47.2 completo

--- Ejercicio_47/index.js
+++ Ejercicio_47/index.js
@@ -2,14 +2,23 @@
 1) Programa una función que cuente el número de caracteres de
 una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
 */
-const userData = ["Hola mundo", 1, "Hola"];
-const userDataSize = userData.length;
 
-typeof userData === "string"
-  ? console.log(`Los caracteres del string son: ${userDataSize}`)
-  : console.log("Ingresa un dato tipo String");
+function numberOfCharacters(string) {
+  const userData = string;
+  const userDataSize = userData.length;
 
-/*
-2) Programa una función que te devuelva el texto recortado según el número
-de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
-*/
+  typeof userData === "string"
+    ? console.log(`Los caracteres del string son: ${userDataSize}`)
+    : console.log("Ingresa un dato tipo String");
+}
+
+numberOfCharacters("Hola Mundo");
+
+/* 2) Programa una función que te devuelva el texto recortado según el número
+ de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola". */
+
+function cutText(inputText, numbertocut) {
+  const newText = inputText.slice(0, numbertocut);
+  console.log(newText);
+}
+cutText("Hola Mundo", 4);
