42ae63e02493f9ecfcb6fd5bf3eff31635fc458b
dcuevas24 <ivandaniel1224@gmail.com>
2021-11-29 09:28:39 -0500

    Solución ejercicio 47.1 completo

---
+++ Ejercicio_47/index.js
@@ -0,0 +1,15 @@
+/*
+1) Programa una función que cuente el número de caracteres de
+una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+*/
+const userData = ["Hola mundo", 1, "Hola"];
+const userDataSize = userData.length;
+
+typeof userData === "string"
+  ? console.log(`Los caracteres del string son: ${userDataSize}`)
+  : console.log("Ingresa un dato tipo String");
+
+/*
+2) Programa una función que te devuelva el texto recortado según el número
+de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+*/
