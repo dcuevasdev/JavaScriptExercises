60194e6aaed164bd1d8f55e0d5779b4334a7b56b
dcuevas24 <ivandaniel1224@gmail.com>
2021-12-04 10:24:35 -0500

    Solución ejercicio 48.2

--- Ejercicio_48/index.js
+++ Ejercicio_48/index.js
@@ -1,5 +1,5 @@
 /*
-5) Programa una función que invierta las palabras de una cadena de texto, pe.
+1) Programa una función que invierta las palabras de una cadena de texto, pe.
 miFuncion("Hola Mundo") devolverá "odnuM aloH".
 */
 const investWord = (string = undefined) => {
@@ -18,4 +18,35 @@ const investWord = (string = undefined) => {
   return console.log(newWord);
 };
 
-investWord(56);
+investWord("Hola Mundo");
+
+/*
+2) Programa una función para contar el número de veces que se repite una
+palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo")
+devolverá 2
+*/
+const repeatWord = (string = undefined, repeat) => {
+  if (string === undefined) return console.warn("No ingresaste ningún dato");
+
+  const validatorNumber = typeof string;
+  if (validatorNumber === "number")
+    return console.warn("Ingresa un valor válido que sea tipo string");
+
+  const transformString = string.toLowerCase();
+  const transformRepeat = repeat.toLowerCase();
+
+  const arrayWord = transformString.split(" ");
+  let counter = 0;
+
+  arrayWord.forEach((element) => {
+    if (element === transformRepeat) {
+      counter = counter + 1;
+    }
+  });
+
+  if (counter === 0) return console.log("Ninguna palabra se repite");
+
+  return console.log(`La palabra ${repeat} se repite ${counter} veces`);
+};
+
+repeatWord("hola mundo adios mundo", "mundo");
