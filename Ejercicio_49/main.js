4e2515f225ae3c0e40c603355f2f34c939beb3a5
dcuevas24 <ivandaniel1224@gmail.com>
2021-12-08 11:58:57 -0500

    Solución ejercicio 49.2

--- Ejercicio_49/main.js
+++ Ejercicio_49/main.js
@@ -8,3 +8,20 @@ const random = (num1, num2) => {
 };
 
 random(501, 600);
+
+/*
+2) Programa una función que reciba un número y evalúe si es capicúa
+o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002)
+devolverá true.
+*/
+const capicua = (num = undefined) => {
+  if (typeof num === "string" || num === undefined)
+    return console.log("Ingresa un dato de tipo number");
+
+  const convertToString = String(num);
+  const invertNumber = convertToString.split("").reverse().join("");
+
+  invertNumber === convertToString ? console.log(true) : console.log(false);
+};
+
+capicua(9889);
