2753c760fa1442a7a944da03c489a2fa7d1c5f2f
dcuevas24 <ivandaniel1224@gmail.com>
2022-01-08 14:07:25 -0500

    Solución ejercicio 53.3

--- Ejercicio_53/main.js
+++ Ejercicio_53/main.js
@@ -57,3 +57,43 @@ maxMin("Hola");
 maxMin({ one: 1, two: 2 });
 maxMin([1, 4, 5, 99, -60]);
 maxMin([]);
+
+/*
+3)Programa una función que dado un array de números devuelva un objeto con 2
+arreglos en el primero almacena los números pares y en el segundo los
+impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0],
+impares: [1,3,5,7,9]}.
+*/
+const evenOdd = (arrayNumbers) => {
+  if (arrayNumbers === undefined) return console.log("Debes ingresar un Array");
+
+  if (Object.prototype.toString.call(arrayNumbers) === "[object Object]")
+    return console.log(`El valor que ingresaste no es válido`);
+
+  if (typeof arrayNumbers === "string" || typeof arrayNumbers === "number")
+    return console.log(`El valor "${arrayNumbers}" no es un Array`);
+
+  if (arrayNumbers.length === 0) return console.log("Ingresa datos al Array");
+
+  const classification = {
+    even: [],
+    odd: [],
+  };
+
+  const sortOrder = arrayNumbers.map((number) => {
+    if (number % 2 === 0) {
+      classification.even.push(number);
+    } else {
+      classification.odd.push(number);
+    }
+  });
+
+  console.log(classification);
+};
+
+evenOdd();
+evenOdd(1);
+evenOdd("Hola");
+evenOdd({});
+evenOdd([]);
+evenOdd([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
